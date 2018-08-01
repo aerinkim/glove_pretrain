b92d8183f677336fe19bca3f22237a0c1543a6de
Aerin Kim <ohmyheroine@hotmail.com>
2018-08-01 15:57:18 -0700

    flattening done.

--- flatten_glove.py
+++ flatten_glove.py
@@ -1,15 +1,10 @@
-import pickle
-
-
-def flatten_glove(path):
-
+def flatten_glove():
 	with open("flattened.glove.840d.300.txt", mode="w") as outfile: 
-
-		with open('Glove.840d.300.txt', encoding="utf8") as f:
+		with open('~/Desktop/squad_vteam/data/glove.840B.300d.txt', encoding="utf8") as f:
 			for line in f:
 				mylist = line.split()[1:] # Removing the word (key) and write only values
-			    for s in mylist:
-			        outfile.write("%s\n" % s)
+				for s in mylist:
+					outfile.write("%s\n" % s)
 
 if __name__ == '__main__':
 	flatten_glove()
