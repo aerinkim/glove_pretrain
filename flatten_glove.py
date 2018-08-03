9c73b1c9811459d634b66f927ff309dd5117943f
Aerin Kim <ohmyheroine@hotmail.com>
2018-08-03 15:37:28 -0700

    implemented error checking - isdigit

--- flatten_glove.py
+++ flatten_glove.py
@@ -1,15 +1,20 @@
-import pickle
-
-
-def flatten_glove(path):
-
+def flatten_glove():
+	i=0
 	with open("flattened.glove.840d.300.txt", mode="w") as outfile: 
-
-		with open('Glove.840d.300.txt', encoding="utf8") as f:
+		with open('../squad_vteam/data/glove.840B.300d.txt', encoding="utf8") as f:
 			for line in f:
-				mylist = line.split()[1:] # Removing the word (key) and write only values
-			    for s in mylist:
-			        outfile.write("%s\n" % s)
+				tokens = line.split() # Removing the word (key) and write only values
+				#print(mylist[0])
+				#if len(mylist) != 300:
+				#	print(i, line.split()[0])
+				tokens = tokens[-300:]
+				for s in tokens:
+					if s.isdigit():
+						s=float(s)/2
+						outfile.write("%s\n" % s)
+					else:
+						raise ValueError('is not digit!')
+				i+=1
 
 if __name__ == '__main__':
 	flatten_glove()
