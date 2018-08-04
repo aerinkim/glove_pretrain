534e7828b6c5964813779a320b6f93c30229bacc
Aerin Kim <ohmyheroine@hotmail.com>
2018-08-04 14:24:59 -0700

    added check_duplicate.py file

---
+++ check_duplicate.py
@@ -0,0 +1,25 @@
+
+def check_duplicate():
+	set_=set()
+	with open('../GloVe/build/new_vocab.txt', encoding="utf8") as f:
+		for line in f:
+			elems = line.split()
+			token = '_'.join(elems[0:-1])
+			if token in set_:
+				print ("dup", token)
+			else:
+				set_.add(token)
+
+
+def check_duplicate2():
+	set_=set()
+	with open('../v1_squad_vteam/data/glove.840B.300d.txt', encoding="utf8") as f:
+		for line in f:
+			elems = line.split()
+			token = '_'.join(elems[0:-300]) 
+			if token in set_:
+				print ("dup", token)
+			else:
+				set_.add(token)
+
+check_duplicate()
