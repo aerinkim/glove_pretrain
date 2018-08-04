976b7a5bbeae13a587b5fc33639f815904d1717a
Aerin Kim <ohmyheroine@hotmail.com>
2018-08-03 23:05:23 -0700

    add utf8 tag on flattening as well

--- flatten_glove.py
+++ flatten_glove.py
@@ -1,7 +1,7 @@
 def flatten_glove():
 
 	i=0
-	with open("flattened.glove.840d.300.txt", mode="w") as outfile: 
+	with open("flattened.glove.840d.300.txt", mode="w" , encoding="utf8" ) as outfile: 
 		with open('../squad_vteam/data/glove.840B.300d.txt', encoding="utf8") as f:
 			for line in f:
 				tokens = line.split() # Removing the word (key) and write only values
