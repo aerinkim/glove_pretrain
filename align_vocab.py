abd897c2e83fbe384f62cbc3ef8a0555d3785acf
Aerin Kim <ohmyheroine@hotmail.com>
2018-08-04 14:24:43 -0700

    glove.840b contains duplicated keys. added int + ___ to differentiate the keys

--- align_vocab.py
+++ align_vocab.py
@@ -1,21 +1,39 @@
 
 # We have to align the sequence of vocab.txt as same as glove.840d.300.txt
 
+# Also  glove.840d.300.txt contains duplicated keys. We have to remove them.
+
+
 def create_new_vocab_txt(unigram_count_dictionary):
 	unigram_count_dictionary = create_unigram_count_dictionary() 
 	# We have to align the sequence of vocab.txt as same as glove.840d.300.txt
-	with open("new_vocab.txt", mode="w" , encoding="utf8") as outfile: 
+	with open("../GloVe/build/new_vocab.txt", mode="w" , encoding="utf8") as outfile: 
 		with open('../squad_vteam/data/glove.840B.300d.txt', encoding="utf8") as f:
+			i=0
+			j=0
+			tokens_seen_before=set()
+
 			for line in f:
 				elems = line.split()
-				token = ' '.join(elems[0:-300])
+
+				if len(elems) != 301:
+					print (i, elems[:-300])
+				
+				token = '_'.join(elems[0:-300])
+
+				if token in tokens_seen_before:
+					token = token+str(j)+'___'
+					j+=1
+				else:
+					tokens_seen_before.add(token)
 
 				if token in unigram_count_dictionary.keys():
 					val = unigram_count_dictionary[token]
 				else:
-					print (token," is not found!")
+					#print (token," is not found!")
 					val = 10
 				outfile.write("%s %s\n" %(token, val ))
+				i+=1
 
 
 def create_unigram_count_dictionary():
