6100b872b8be2a286d9d4c5572a871092cc751e0
Aerin Kim <ohmyheroine@hotmail.com>
2018-08-05 13:56:36 -0700

    make the output file written in Glove/build

--- flatten_glove.py
+++ flatten_glove.py
@@ -1,20 +1,23 @@
 def flatten_glove():
 
 	i=0
-	with open("flattened.glove.840d.300.txt", mode="w" , encoding="utf8" ) as outfile: 
-		with open('../squad_vteam/data/glove.840B.300d.txt', encoding="utf8") as f:
+	with open("../GloVe/build/flattened.glove.840d.300.txt", mode="w" , encoding="utf8" ) as outfile: 
+		with open('../v1_squad_vteam/data/glove.840B.300d.txt', encoding="utf8") as f:
 			for line in f:
 				tokens = line.split() # Removing the word (key) and write only values
 				#print(mylist[0])
 				#if len(mylist) != 300:
 				#	print(i, line.split()[0])
-				tokens = tokens[-300:]
-				for s in tokens:
-					if s.isdigit():
-						s=float(s)/2
-						outfile.write("%s\n" % s)
-					else:
-						raise ValueError('is not digit!')
+				dimensions = tokens[-300:]
+
+				for s in dimensions:
+				
+					try:
+					    s=float(s)/2
+					    outfile.write("%s\n" % s)
+					except ValueError:
+					    print ("Not a float")
+
 				i+=1
 
 if __name__ == '__main__':
